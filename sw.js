e912c5438cfa6c1c9bf96e7c695fb239ad45cd1d
juanjuanjo1980 <63320897+juanjuanjo1980@users.noreply.github.com>
2026-06-05 18:45:35 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Service Worker minimo - sin cache para evitar errores
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-// No interceptamos fetch - dejamos que el navegador maneje todo
